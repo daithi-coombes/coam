7c6264f1b5b14879c7195f3f3a5012eb6b4e522a
daithi-coombes <webeire@gmail.com>
2013-05-27 01:29:45 +0100

    hide sidebar

--- functions.php
+++ functions.php
@@ -1,1 +1,12 @@
 <?php
+
+//hide sidebars
+function coam_hide_sidebars(){
+	foreach(array(
+		'sidebar-1',
+		'sidebar-2',
+		'sidebar-3'
+	) as $sidebar)
+		unregister_sidebar($sidebar);
+}
+add_action('widgets_init', 'coam_hide_sidebars',10);
